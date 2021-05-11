007f6c65266bb4872354d1deedcdb08cdcc8ffcb
Maryshev Andrei <tecak48rus@gmail.com>
2021-05-11 18:01:10 +0300

    Generate hash for file and give NAME file

--- generatehash.cpp
+++ generatehash.cpp
@@ -1,14 +1,62 @@
+//******************************************************************************
 #include "generatehash.h"
 #include "ui_generatehash.h"
-
+//******************************************************************************
 GenerateHash::GenerateHash(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::GenerateHash)
 {
     ui->setupUi(this);
 }
-
+//******************************************************************************
 GenerateHash::~GenerateHash()
 {
     delete ui;
 }
+//******************************************************************************
+QByteArray GenerateHash::myhashFunction(QString pathFile)
+{
+    if (QFile::exists(pathFile))
+    {
+        QFile fileData(pathFile);
+        QFileInfo fileInfo(fileData.fileName());
+        nameFile = fileInfo.fileName();
+        if(fileData.open(QIODevice::ReadOnly))
+        {
+            QCryptographicHash objHashByte(Algorythm);
+            if(objHashByte.addData(&fileData))
+            {
+                QString objHashString = (objHashByte.result()).toHex();
+                ui->txteditresult->setPlainText(objHashString);
+                hashFile = objHashByte.result();
+                return hashFile;
+            }
+        }
+        return hashFile;
+    }else return 0;
+
+}
+//******************************************************************************
+void GenerateHash::myhashWriteInfo(QString nameFile, QString hashFile)
+{
+    qDebug() << nameFile << hashFile;
+}
+//******************************************************************************
+void GenerateHash::on_pushchooseFile_clicked()
+{
+    ui->PathFile->setText(QFileDialog::getOpenFileName(this));
+}
+//******************************************************************************
+void GenerateHash::on_pushgenerate_clicked()
+{
+    pathFile = ui->PathFile->text();
+    if (pathFile.size() < 1)
+        return;
+    QString curModeHash =  ui->comboBox->currentText();
+    if (curModeHash == "SHA1") Algorythm = QCryptographicHash::Sha1;
+    else if (curModeHash == "MD4") Algorythm = QCryptographicHash::Md4;
+    else if (curModeHash == "MD5") Algorythm = QCryptographicHash::Md5;
+    myhashFunction(pathFile);
+    myhashWriteInfo(nameFile, hashFile);
+}
+//******************************************************************************
--- generatehash.h
+++ generatehash.h
@@ -1,12 +1,16 @@
+//******************************************************************************
 #ifndef GENERATEHASH_H
 #define GENERATEHASH_H
-
+//******************************************************************************
 #include <QMainWindow>
-
+#include <QFileDialog>
+#include <QCryptographicHash>
+#include <QDebug>
+//******************************************************************************
 namespace Ui {
 class GenerateHash;
 }
-
+//******************************************************************************
 class GenerateHash : public QMainWindow
 {
     Q_OBJECT
@@ -15,8 +19,19 @@ public:
     explicit GenerateHash(QWidget *parent = 0);
     ~GenerateHash();
 
+    QByteArray myhashFunction(QString pathFile);
+    void myhashWriteInfo(QString nameFile, QString hashFile);
+
+private slots:
+
+    void on_pushchooseFile_clicked();
+    void on_pushgenerate_clicked();
+
 private:
     Ui::GenerateHash *ui;
+    QString pathFile, nameFile;
+    QCryptographicHash::Algorithm Algorythm;
+    QByteArray hashFile;
 };
 
 #endif // GENERATEHASH_H
